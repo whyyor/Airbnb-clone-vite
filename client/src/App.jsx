55da166d06a1d142ab99fc87558040df28e3587d
whyyor <whyyor@gmail.com>
2023-02-28 13:25:26 +0530

    Add react-router-dom for multiple routes
    wrapped app inside browserouter in main.jsx
    
    created a route in app.jsx and moved content to indexpage.jsx in pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,13 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import IndexPage from './pages/IndexPage'
 
 function App() {
-  return <div>test</div>
+  return (
+    <Routes>
+      <Route index element={<IndexPage />} />
+    </Routes>
+  )
 }
 
 export default App
