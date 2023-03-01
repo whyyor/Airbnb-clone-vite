4a222ad9d786e5c0c46fe39eeb0099dd83c8b5bc
whyyor <whyyor@gmail.com>
2023-03-01 16:18:26 +0530

    change test route to post register route
    now it sends register route with name,email,pass
    
    change indexjs to parse json and now it returns passed name,email,password

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 
+app.use(express.json())
+
 app.use(
   cors({
     credentials: true,
@@ -11,6 +13,11 @@ app.use(
 
 app.get('/test', (req, res) => {
   res.json('test ok')
+})
+
+app.post('/register', (req, res) => {
+  const { name, email, password } = req.body
+  res.json({ name, email, password })
 })
 
 app.listen(4000)
