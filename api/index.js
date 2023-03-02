de051f7601db5fc7d6cea72718d58c061782562d
whyyor <whyyor@gmail.com>
2023-03-02 13:05:04 +0530

    Add mongoose to project
    Register database on cloud
    
    Add .env and dotenv package in yarn to get url
    
    require dotenv.config in project and require mongoose
    
    Use mongoose.connect to use that url using our env file

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config()
 const app = express()
 
 app.use(express.json())
+//this will help us parse json so that we can access variables
 
 app.use(
   cors({
@@ -10,6 +13,8 @@ app.use(
     origin: 'http://localhost:5173',
   }),
 )
+
+mongoose.connect(process.env.MONGO_URL)
 
 app.get('/test', (req, res) => {
   res.json('test ok')
