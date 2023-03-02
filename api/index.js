84b82f2ce573af1a7afa96a55f2ebbf525034b1d
whyyor <whyyor@gmail.com>
2023-03-02 13:26:56 +0530

    add user.js in api folder to produce schema for user api
    use schema from mongoose and create a new schema
    
    create model using mongoose and use created schema
    
    export this model
    
    add bcrypt to our index.js
    
    use becrypt to create a salt using 12 rounds
    
    in password encrypt password using bcrypt.hashsync
    
    store user.create in userdoc and return that user
    
    make our post register async function

--- api/index.js
+++ api/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const UserModel = require('./models/User')
 require('dotenv').config()
 const app = express()
+
+const bcryptSalt = bcrypt.genSalt(12)
 
 app.use(express.json())
 //this will help us parse json so that we can access variables
@@ -20,9 +24,14 @@ app.get('/test', (req, res) => {
   res.json('test ok')
 })
 
-app.post('/register', (req, res) => {
+app.post('/register', async (req, res) => {
   const { name, email, password } = req.body
-  res.json({ name, email, password })
+  const userDoc = await User.create({
+    name,
+    email,
+    password: bcrypt.hashSync(password, bcryptSalt),
+  })
+  res.json(userDoc)
 })
 
 app.listen(4000)
