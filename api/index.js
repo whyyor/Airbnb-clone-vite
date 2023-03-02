53c6aa89ab2a8dc06939d41284a1793849de4f0d
whyyor <whyyor@gmail.com>
2023-03-02 21:56:08 +0530

    we install jsonwebtoken to make cookies
    const jwt = requrire(jsonwebcookie) for using cookie
    
    now if pass is ok we sign cookie with email and unique mongoose id
    
    we can use await or 4 params so here we used 4 params
    
    in 4th param we make a callback function that returns cookie using res.cookie
    
    if any error is found our callback function throws error

--- api/index.js
+++ api/index.js
@@ -2,11 +2,13 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const User = require('./models/User')
 require('dotenv').config()
 const app = express()
 
 const bcryptSalt = bcrypt.genSaltSync(10)
+const jwtSecret = 'jfladksjfls'
 
 app.use(express.json())
 //this will help us parse json so that we can access variables
@@ -46,7 +48,15 @@ app.post('/login', async (req, res) => {
     const passOk = bcrypt.compareSync(password, userDoc.password)
     //we compare our password with userDoc password
     if (passOk) {
-      res.json('password ok')
+      jwt.sign(
+        { email: userDoc.email, id: userDoc._id },
+        jwtSecret,
+        {},
+        (err, token) => {
+          if (err) throw err
+          res.cookie('token', token).json('pass ok')
+        },
+      )
     } else {
       res.status(422).json('pass not ok')
     }
