c7126c5804efeb6ae52f28063d0a8fd0e8f05b13
whyyor <whyyor@gmail.com>
2023-03-02 21:47:13 +0530

    make app post login
    in login get email and pass from req.body
    
    find email if email exist check if password is right
    
    on right password res.json(password ok) else not ok

--- api/index.js
+++ api/index.js
@@ -43,7 +43,13 @@ app.post('/login', async (req, res) => {
   const { email, password } = req.body
   const userDoc = await User.findOne({ email })
   if (userDoc) {
-    res.json('found')
+    const passOk = bcrypt.compareSync(password, userDoc.password)
+    //we compare our password with userDoc password
+    if (passOk) {
+      res.json('password ok')
+    } else {
+      res.status(422).json('pass not ok')
+    }
   } else {
     res.json('not found')
   }
