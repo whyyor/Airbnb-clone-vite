3f21b114166744ae48b799f4beecc6b53b7f486e
whyyor <whyyor@gmail.com>
2023-03-05 14:02:21 +0530

    In header if user exist he is redirected to /account
    In logo we now redirect to /

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
   const { user } = useContext(UserContext)
   return (
     <header className="flex space-between justify-between">
-      <a href="" className="flex items-center gap-1">
+      <Link to={'/'} className="flex items-center gap-1">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -22,7 +22,7 @@ export default function Header() {
           />
         </svg>
         <span className="font-bold text-xl">airbnb</span>
-      </a>
+      </Link>
       {/* used heroicons for svg */}
       <div className="flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300">
         <div>Anywhere</div>
@@ -50,7 +50,7 @@ export default function Header() {
         </button>
       </div>
       <Link
-        to={'/login'}
+        to={user ? '/account' : '/login'}
         className="flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md"
       >
         <svg
