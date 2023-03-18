fb7972685e2035d2055d10a367a4b5a8873c64ac
whyyor <whyyor@gmail.com>
2023-03-19 02:17:39 +0530

    Revamp placespage's photosuploader component to a seprate component

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,14 +1,13 @@
 import { Link, useParams } from "react-router-dom";
 import { useState } from "react";
-import axios from "axios";
 import Perks from "../Perks";
+import PhotosUploader from "../PhotosUploader";
 
 export default function PlacesPage() {
   const { action } = useParams();
   const [title, setTitle] = useState("");
   const [address, setAddress] = useState("");
-  const [addedPhotos, setAddedPhotos] = useState([]);
-  const [photoLink, setPhotoLink] = useState("");
+  const [addedPhotos, setAddedPhotos] = useState([])
   const [description, setDescription] = useState("");
   const [perks, setPerks] = useState([]);
   const [extraInfo, setExtraInfo] = useState("");
@@ -33,36 +32,6 @@ export default function PlacesPage() {
     );
   }
 
-  async function addPhotoByLink(ev) {
-    ev.preventDefault();
-    //this will disable automatic reload on adding photo
-    const { data: filename } = await axios.post("/upload-by-link", {
-      link: photoLink,
-    });
-    //because we are return photoname in res.json
-    setAddedPhotos((prev) => {
-      return [...prev, filename];
-      //will store all previous photos and new photo
-    });
-    setPhotoLink("");
-  }
-
-  function uploadPhoto(ev) {
-    const files = ev.target.files;
-    const data = new FormData();
-    for (let i = 0; i < files.length; i++) {
-      data.append("photos", files[i]);
-    }
-    axios.post("/upload", data, {
-      headers: { "Content-type": "multipart/form-data" },
-    }).then((response) => {
-      const { data: filename } = response;
-      setAddedPhotos((prev) => {
-        return [...prev, filename];
-        //will store all previous photos and new photo
-      });
-    });
-  }
 
   return (
     <div>
@@ -111,55 +80,9 @@ export default function PlacesPage() {
               placeholder="address"
             />
             {preInput("Photos", "More = better")}
-            <div className="flex gap-2">
-              <input
-                type="text"
-                value={photoLink}
-                onChange={(ev) => setPhotoLink(ev.target.value)}
-                placeholder={"Add using a link... jpg"}
-              />
-              <button
-                onClick={addPhotoByLink}
-                className="bg-gray-200 px-4 rounded-2xl"
-              >
-                Add&nbsp;Photo
-              </button>
-            </div>
-            <div className="mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
-              {addedPhotos.length > 0 &&
-                addedPhotos.map((imgName) => (
-                  <div className="h-32 flex" key={imgName} >
-                    <img
-                      src={"http://localhost:4000/uploads/" + imgName}
-                      className="rounded-2xl w-full object-cover"
-                      alt=""
-                    />
-                  </div>
-                ))}
-              <label className="h-32 cursor-pointer flex items-center justify-center gap-1 border bg-transparent rounded-2xl p-2 text-2xl text-gray-600">
-                <input
-                  type="file"
-                  multiple
-                  className="hidden"
-                  onChange={uploadPhoto}
-                />
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-8 h-8"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"
-                  />
-                </svg>
-                Upload
-              </label>
-            </div>
+
+            <PhotosUploader addedPhotos={addedPhotos} onChange={setAddedPhotos} />
+
             {preInput("Description", "description of the place")}
             <textarea
               value={description}
