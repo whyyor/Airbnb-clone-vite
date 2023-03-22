eea5f2ff4e187d46be29b045af753adb943e66c6
whyyor <whyyor@gmail.com>
2023-03-22 21:19:21 +0530

    In placepage fix profile photo not visible
    In photosupload add option to remove a photo by filtering out all photos expect current

--- client/src/PhotosUploader.jsx
+++ client/src/PhotosUploader.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
-export default function PhotosUploader({addedPhotos,onChange}) {
-
+export default function PhotosUploader({ addedPhotos, onChange }) {
   const [photoLink, setPhotoLink] = useState("");
 
   async function addPhotoByLink(ev) {
@@ -36,6 +36,11 @@ export default function PhotosUploader({addedPhotos,onChange}) {
     });
   }
 
+  function removePhoto(filename) {
+     onChange([...addedPhotos.filter(photo => photo!== filename)]);
+    //this will filter out the current photo and reutrn a new array with all the other photos left
+  }
+
   return (
     <>
       <div className="flex gap-2">
@@ -55,12 +60,28 @@ export default function PhotosUploader({addedPhotos,onChange}) {
       <div className="mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
         {addedPhotos.length > 0 &&
           addedPhotos.map((imgName) => (
-            <div className="h-32 flex" key={imgName}>
+            <div className="h-32 flex relative" key={imgName}>
               <img
                 src={"http://localhost:4000/uploads/" + imgName}
                 className="rounded-2xl w-full object-cover"
                 alt=""
               />
+              <button onClick={()=>removePhoto(imgName)} className="cursor-pointer absolute bottom-1 right-1 text-white bg-black py-2 bg-opacity-50 rounded-2xl  ">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                  />
+                </svg>
+              </button>
             </div>
           ))}
         <label className="h-32 cursor-pointer flex items-center justify-center gap-1 border bg-transparent rounded-2xl p-2 text-2xl text-gray-600">
