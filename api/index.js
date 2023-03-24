d3c20ce1d4fe99d041c3a3f17109b132b4088391
whyyor <whyyor@gmail.com>
2023-03-24 19:11:16 +0530

    In index.js add path for get places
    Add some css to layout.jsx
    
    In indexpage.jsx show all the places

--- api/index.js
+++ api/index.js
@@ -201,4 +201,8 @@ app.put("/places", async (req, res) => {
   });
 });
 
+app.get('/places',async (req, res) => {
+    res.json(await Place.find())
+})
+
 app.listen(4000);
--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,3 +1,37 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function IndexPage() {
-  return <div>Index page here</div>
+  const [places, setPlaces] = useState([]);
+  useEffect(() => {
+    axios.get("/places").then((response) => {
+      setPlaces([
+        ...response.data,
+        ...response.data,
+        ...response.data,
+        ...response.data,
+      ]);
+    });
+  }, []);
+  return (
+    <div className="mt-8 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-y-8 gap-x-6">
+      {places.length > 0 &&
+        places.map((place) => (
+          <div>
+            <div className="bg-gray-500 rounded-2xl flex">
+              {place.photos?.[0] && (
+                <img
+                  className="rounded-2xl object-cover aspect-square"
+                  src={"http://localhost:4000/uploads/" + place.photos?.[0]}
+                  alt=""
+                  srcset=""
+                />
+              )}
+            </div>
+            <h2 className="text-sm truncate mt-2 leading-4">{place.title}</h2>
+            <h3 className="font-bold">{place.address}</h3>
+          </div>
+        ))}
+    </div>
+  );
 }
